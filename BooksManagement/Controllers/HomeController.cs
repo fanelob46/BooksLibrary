256bd15bd60e3d6b6cb9b55b6c5e7d6d91b74daa
fanelob46 <fanelob46@gmail.com>
2024-05-13 14:29:27 +0200

    Home page display and functionality

--- BooksManagement/Controllers/HomeController.cs
+++ BooksManagement/Controllers/HomeController.cs
@@ -1,21 +1,32 @@
+using BooksManagement.Data;
 using BooksManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net;
 
 namespace BooksManagement.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDBContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDBContext dBContext)
         {
             _logger = logger;
+            _dbContext = dBContext;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Book> booksList = _dbContext.Books.ToList();
+            return View(booksList);
+        }
+
+        public IActionResult Details(int BookId)
+        {
+            Book? book = _dbContext.Books.FirstOrDefault(u => u.BookId == BookId);
+            return View(book);
         }
 
         public IActionResult Privacy()
